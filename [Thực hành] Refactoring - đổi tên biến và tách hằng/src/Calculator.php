af36015ba0d79294a11033478401c976ee7817b4
ducmanh27101994 <ducmanh27101994@gmail.com>
2020-06-30 15:52:05 +0700

    update

---
+++ [Thực hành] Refactoring - đổi tên biến và tách hằng/src/Calculator.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phanluan
+ * Date: 25/10/2018
+ * Time: 00:02
+ */
+
+class Calculator
+{
+    const ADDITION = '+';
+    const SUBTRACTION = '-';
+    const MULTIPLICATION = '*';
+    const DIVISION = '/';
+
+    public function calculate($firstOperand, $secondOperand, $operator) {
+        switch ($operator) {
+            case '' . self::ADDITION . '':
+                return $firstOperand + $secondOperand;
+            case '' . self::SUBTRACTION . '':
+                return $firstOperand - $secondOperand;
+            case '' . self::MULTIPLICATION . '':
+                return $firstOperand * $secondOperand;
+            case '' . self::DIVISION . '':
+                if ($secondOperand != 0) {
+                    return $firstOperand / $secondOperand;
+                } else {
+                    echo("Can not divide by 0");
+                    break;
+                }
+            default:
+                echo ("Unsupported operation");
+        }
+    }
+}
